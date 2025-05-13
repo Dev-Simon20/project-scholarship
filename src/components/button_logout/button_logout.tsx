08a91bf21b16f9ae5782bb55c515839d1fd1d995
Dev-Simon20 <jeanpierks6@gmail.com>
2025-05-13 17:22:34 -0500

    Was added button logout

---
+++ src/components/button_logout/button_logout.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { signOut } from "next-auth/react";
+import { Button } from "../ui/button";
+
+const LogoutButton = () => {
+   const handleOut = async () => {
+      await signOut({
+         redirect: true,
+         redirectTo: "/projects/my-little-shop/login",
+      });
+   };
+   return <Button onClick={() => handleOut()}>LogOut</Button>;
+};
+export default LogoutButton;
