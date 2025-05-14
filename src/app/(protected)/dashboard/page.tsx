c879583ac676435b70b74866056576cf0c284f75
Dev-Simon20 <jeanpierks6@gmail.com>
2025-05-14 07:42:08 -0500

    Extend session whit the atributes: id,names,roles

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { auth } from "@nextAuth/auth";
 import LogoutButton from "@/components/button_logout/button_logout";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
    const session = await auth();
-   
+   if (!session) redirect("/log-in");
    return (
       <div className="container">
          <pre>{JSON.stringify(session, null, 2)}</pre>
